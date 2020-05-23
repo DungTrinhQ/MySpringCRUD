a5f05153e5f35d602f5745a61003afaca5018d24
Dung <dungtrinh0702@gmail.coom>
2020-05-23 21:36:45 +0700

    tạo interface UserService

---
+++ src/main/java/com/codegym/myspringcrud/service/UserService.java
@@ -0,0 +1,16 @@
+package com.codegym.myspringcrud.service;
+
+import com.codegym.myspringcrud.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<User> getAllUser();
+
+    void saveUser(User user);
+
+    void deleteUser(Long id);
+
+    Optional<User> findUserById(Long id);
+}
