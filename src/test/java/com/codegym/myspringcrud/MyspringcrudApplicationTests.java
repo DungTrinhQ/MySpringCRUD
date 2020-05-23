b7a644e9ddde70b7f1a271c6d339d53a3cbbd00a
Dung <dungtrinh0702@gmail.coom>
2020-05-23 21:10:53 +0700

    tạo project

---
+++ src/test/java/com/codegym/myspringcrud/MyspringcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.myspringcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
